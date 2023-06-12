f02be0ccf07eff266291b03a39d0d448ab0e39e0
Krishna <pjrkrishna@gmail.com>
2023-06-12 17:52:52 +0530

    added project code

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { fadeIn, slideIn, staggerContainer } from "../../utils/motion";
+import css from "./Hero.module.scss";
+import { motion } from "framer-motion";
+const Hero = () => {
+  return (
+    <section className={`paddings ${css.wrapper}`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`innerWidth ${css.container}`}
+      >
+        <div className={css.upperElements}>
+          <motion.span className="primaryText" variants={fadeIn("right", "tween", 0.2, 1)}>
+            Hey There,
+            <br />
+            I'm Krishna Potarlanka.
+          </motion.span>
+          <motion.span className="secondaryText"variants={fadeIn("left", "tween", 0.4, 1)}>
+            Health Care IT{" "}
+            <br />
+            Techno Functional Consultant{" "}
+            {/* <br />
+            HIS/EMR Implementations{" "}
+            <br />
+            IT Infra Structure Management{" "}
+            <br />
+            Project Management{" "}
+            <br />
+            MS SQL, Oracle, PostgreSQL{" "}
+            <br />
+            Database Desinging{" "}
+            <br />
+            Fullstack Developer{" "} */}
+          </motion.span>
+        </div>
+
+        <motion.div
+          variants={fadeIn("up", "tween", 0.3, 1)}
+          className={css.person}
+        >
+          <motion.img style={{borderRadius:'50%', width: 400, height: 400}} variants={slideIn("up", "tween", 0.5, 1.3)} src="./pofile.png" alt="" />
+        </motion.div>
+
+        <a className={css.email} href="mailto:pjrkrishna@gmail.com">
+        pjrkrishna@gmail.com
+        </a>
+
+        <div className={css.lowerElements}>
+          <motion.div variants={fadeIn("right", "tween", 0.3, 1)} className={css.experience}>
+            <div className="primaryText">16+</div>
+            <div className="secondaryText">
+              <div>Years</div>
+              <div>Experience</div>
+            </div>
+          </motion.div>
+
+          <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className={css.certificate}>
+            <img src="./mcitp1.png" alt=""/>
+            <span>Microsoft Certified IT Professional</span>
+            <span>Priciple Architect</span>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/utils/data.js
@@ -0,0 +1,208 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  // {
+  //   name: "Website Design",
+  //   projects: 76,
+  //   icon: HiOutlineDesktopComputer,
+  //   bg: "#286F6C",
+  // },
+  // {
+  //   name: "Mobile App Design",
+  //   projects: 63,
+  //   icon: CiMobile1,
+  //   bg: "#EEC048",
+  // },
+  // {
+  //   name: "Brand Identity",
+  //   projects: 47,
+  //   icon: MdWorkspacesOutline,
+  //   bg: "#F26440",
+  // },
+  {
+    name: "Education",
+    projects: "Master of Computer Applications",
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Expertise",
+    projects: "IT Infrastructure Management,DevOps",
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Project Management",
+    projects: "HIS/EMR Implementations, Health Care Apps Development",
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "Techinical Skills",
+    projects: "SQL, TSQL, Pl/SQL, HTML, CSS, JavaScript, Python, Django, Rest API's, ReactJS",
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+];
+
+export const WhatDoIHelp = [
+  "Over 16 years’ experience as Health Care IT professional with strong experience in managing enterprise implementation of healthcare IT infrastructure and specialized in the implementation of EMR/HIS and healthcare apps to the Hospitals.  Executed health care projects in design and implementations for various hospitals, and successfully implemented in Hospital EMR/HIS at different locations across the Telangana and Andhra Pradesh. Trained Doctors, Paramedics, Operation Teams, Billing staff, System administrators, and other employee’s usage of EMR and HIS",
+  "Worked closely with CSC’s I-soft HIS, CGSL Neura HIS, LekhiSoft EMR, Shivam Soft HMS, Remedi HMS, Arcus HIS, Medeaz.com, Docbizz.com, Opoto.in, Rainbow VC, and other E-commerce and CRM Projects. Working as SME and Techno Functional Consultant between Hospitals and IT Developers",
+];
+
+export const workExp = [
+  {
+    place: "Rarinbow Children's Medicare Ltd, Hyderabad",
+    tenure: "Feb 2022 - Mar 2023",
+    role: "Sr. Manager, Group Level",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "Medeaz Health Care Systems, Hyderabad",
+    tenure: "Feb 2018 - Mar 2022",
+    role: "Principle Architect and IT Head",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "Rainbow Children's Hospitals, Hyderabad",
+    tenure: "Feb 2017 - Mar 2018",
+    role: "HIS Support Lead, Group Level",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "Continental Hospitals, Hyderabad",
+    tenure: "Aug 2014 - Sep 2017",
+    role: "Deputy Manager",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "GCM Tech Solutions, Hyderabad",
+    tenure: "May 2012 - Oct 2014",
+    role: "SQL Developer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "Gouatami Eye Institute, Rajahmundry",
+    tenure: "Oct 2009 - Mar 2012",
+    role: "IT-Manager",
+    detail:
+      "A visual desginer dsesign2 for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "Sai Bhavani Super Speciality Hospital, Hyderabad",
+    tenure: "Aug 2006 - Sep 2009",
+    role: "EDP In-Charge",
+    detail:
+      "A visual desginer dsesign2 for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+];
+
+export const comments = [
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
